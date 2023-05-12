d1a2689d64e3850efb8c29677ef0267d7cff0972
Alexandre Sohne Vieira <asv.tech@outlook.com>
2023-05-12 17:11:07 -0300

    Areas - adding layout and routes

--- src/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
+++ src/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DevIO.UI.Site.Areas.Produtos.Controllers
+namespace DevIO.UI.Site.Modulos.Produtos.Controllers
 {
     [Area("Produtos")]
     public class CadastroController : Controller
